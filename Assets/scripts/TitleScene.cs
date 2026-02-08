738a69af6d68245e87e50ee455195a17c8be9a68
Jack High <droLcitsacraS@gmail.com>
2026-02-08 00:25:50 -0500

    ui changes
    changed fonts
    organized assets
    flashing text

--- Assets/scripts/TitleScene.cs
+++ Assets/scripts/TitleScene.cs
@@ -8,6 +8,7 @@ public class TitleScene : MonoBehaviour
 {
 
     public GameObject[] textObjects;
+    public GameObject flashingText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,15 +20,26 @@ public class TitleScene : MonoBehaviour
         StartCoroutine(TitleText());
     }
 
-    IEnumerator TitleText() // when the scene loads this "loads" in the ui elements 
+    IEnumerator TitleText() 
     {
-        foreach (GameObject startText in textObjects)
+        foreach (GameObject startText in textObjects) // when the scene loads this "loads" in the ui elements 
         {
             startText.SetActive(true);
             yield return new WaitForSeconds(.05f);
         }
+
+        while (true) // this flashes text
+        {
+           flashingText.SetActive(false);
+           yield return new WaitForSeconds(.25f);
+
+           flashingText.SetActive(true);
+           yield return new WaitForSeconds(.75f); 
+        }
     }
-// this is where all the button linking is going to go
+
+
+                        // this is where all the button linking is going to go
     public void StartGame()
     {
         SceneManager.LoadScene(1);
