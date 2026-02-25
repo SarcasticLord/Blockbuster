9a591d00f6afa183537bda97da814c567972e0eb
Jack Hi <droLcitsacraS@gmail.com>
2026-02-25 18:54:09 -0500

    ui updates windows load effect

--- Assets/scripts/DragandDrop.cs
+++ Assets/scripts/DragandDrop.cs
@@ -6,9 +6,6 @@ public class DragandDrop : MonoBehaviour, IBeginDragHandler, IDragHandler//, IEn
 {
 
     private RectTransform rectTransform;
-    public string cupSnap;
-
-    public float snapDistance = 80f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
--- Assets/scripts/WindowManager.cs
+++ Assets/scripts/WindowManager.cs
@@ -9,31 +9,88 @@ public class WindowManager : MonoBehaviour
 
     public GameObject myComputerWindow;
     public GameObject internetExplorerWindow;
+    public GameObject statsWindow;
+
+    public GameObject[] MCText;
+    public GameObject[] InternetText;
+    public GameObject[] StatsText;
+
 
       // open/close windows in "os mode" open/close windows in "os mode" open/close windows in "os mode" open/close windows in "os mode" open/close windows in "os mode"
     void Start()
     {
-        myComputerWindow.SetActive(false);
-        internetExplorerWindow.SetActive(false);
-        
+        //myComputerWindow.SetActive(false);
+        //internetExplorerWindow.SetActive(false);
+        //statsWindow.SetActive(false);
+
+        ResetText(MCText);
+        ResetText(InternetText);
+        ResetText(StatsText);
+    }
+
+    void ResetText(GameObject[] textArray) // keeps the text hidden when the ui isnt active
+    {
+        foreach (GameObject loadText in textArray)
+        {
+            loadText.SetActive(false);
+        }
+    }
+    IEnumerator WindowText(GameObject[] textArray) 
+    {
+        foreach (GameObject loadText in textArray) 
+        {
+            loadText.SetActive(true);
+            yield return new WaitForSeconds(.05f);
+        }
+
     }
     public void OpenMCwindow() // OPENS SETTINGS WINDOW 
     {
         myComputerWindow.SetActive(true);
+
+        ResetText(MCText);
+        StartCoroutine(WindowText(MCText));
     }
 
     public void CloseMCwindow() // CLOSE WINDOW
     {
+        
         myComputerWindow.SetActive(false);
+
+        ResetText(MCText);
     }
 
     public void OpenInternetExplorer() // OPENS internet  WINDOW 
     {
+        
         internetExplorerWindow.SetActive(true);
+
+        ResetText(InternetText);
+        StartCoroutine(WindowText(InternetText));
     }
 
     public void CloseInternetExplorer() // close internet window
     {
+        
         internetExplorerWindow.SetActive(false);
+
+        ResetText(InternetText);
+    }
+
+    public void OpenStatsWindow() // OPENS stats  WINDOW 
+    {
+        
+        statsWindow.SetActive(true);
+
+        ResetText(StatsText);
+        StartCoroutine(WindowText(StatsText));
+    }
+
+    public void CloseStatsWindow() // close stats window
+    {
+        
+        statsWindow.SetActive(false);
+
+        ResetText(StatsText);
     }
 }
